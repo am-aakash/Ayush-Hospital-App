48cee5778f8347a64d1dd680cc7326f7514b4c2c
Aakash <akashchaudhary12121@gmail.com>
2022-03-05 01:02:08 +0530

    Splash screen UI done

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -16,60 +16,113 @@ class SplashScreen extends StatelessWidget {
     // SystemChrome.setEnabledSystemUIOverlays([]);
     return Scaffold(
       backgroundColor: Colors.lime[200], //COLORS.primaryCol,
-      body: Container(
-        height: SizeConfig.screenHeight,
-        width: SizeConfig.screenWidth,
-        decoration: BoxDecoration(
-          color: COLORS.primaryCol,
-          borderRadius: BorderRadius.only(
-            bottomRight: Radius.circular(SizeConfig.blockWidth * 70),
-            // topRight: Radius.circular(SizeConfig.blockWidth * 20),
-          ),
-        ),
-        child: Stack(
-          children: <Widget>[
-            Positioned(
-              bottom: SizeConfig.blockHeight * 3,
-              right: SizeConfig.blockWidth * 4,
-              child: InkWell(
-                onTap: () {},
-                // splashColor: Colors.blueGrey[800],
-                // splashFactory: InkRipple.splashFactory,
-                // radius: SizeConfig.blockWidth * 20,
-                child: Container(
-                  child: Row(
-                    children: [
-                      Text(
-                        'CONTINUE',
-                        style: TextStyle(
-                          color: Colors.blueGrey[800],
-                          fontWeight: FontWeight.bold,
-                          // height: SizeConfig.blockHeight * 0,
-                        ),
-                      ),
-                      Icon(
-                        AntIcons.caretRightOutlined,
+      body: Stack(
+        children: <Widget>[
+          Positioned(
+            bottom: SizeConfig.blockHeight * 3,
+            right: SizeConfig.blockWidth * 4,
+            child: InkWell(
+              onTap: () {},
+              // splashColor: Colors.blueGrey[800],
+              // splashFactory: InkRipple.splashFactory,
+              // radius: SizeConfig.blockWidth * 20,
+              child: Container(
+                child: Row(
+                  children: [
+                    Text(
+                      'CONTINUE',
+                      style: TextStyle(
                         color: Colors.blueGrey[800],
-                        size: SizeConfig.blockWidth * 4.5,
+                        fontWeight: FontWeight.bold,
+                        fontSize: SizeConfig.blockWidth * 3.4,
+                        // height: SizeConfig.blockHeight * 0,
                       ),
-                    ],
-                  ),
+                    ),
+                    Icon(
+                      AntIcons.caretRightOutlined,
+                      color: Colors.blueGrey[800],
+                      size: SizeConfig.blockWidth * 4.5,
+                    ),
+                  ],
                 ),
               ),
             ),
-            Positioned(
-              right: 0,
-              bottom: SizeConfig.blockHeight * 40,
-              child: Container(
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage('assets/images/building.png'),
+          ),
+          Container(
+            height: SizeConfig.screenHeight,
+            width: SizeConfig.screenWidth,
+            clipBehavior: Clip.hardEdge,
+            decoration: BoxDecoration(
+              color: COLORS.primaryCol,
+              border: Border.fromBorderSide(BorderSide.none),
+              borderRadius: BorderRadius.only(
+                bottomRight: Radius.circular(SizeConfig.blockWidth * 70),
+                // topRight: Radius.circular(SizeConfig.blockWidth * 20),
+              ),
+            ),
+            child: Stack(
+              children: <Widget>[
+                Positioned(
+                  bottom: -220,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      // color: COLORS.primaryCol,
+                      borderRadius: BorderRadius.only(
+                        bottomRight:
+                            Radius.circular(SizeConfig.blockWidth * 70),
+                        // topRight: Radius.circular(SizeConfig.blockWidth * 20),
+                      ),
+                    ),
+                    child: Image.asset(
+                      'assets/images/bgbuilding.png',
+                      scale: 4,
+                      color: COLORS.primaryCol,
+                      colorBlendMode: BlendMode.color,
+                    ),
                   ),
                 ),
-              ),
-            )
-          ],
-        ),
+                // Container(
+                //   height: SizeConfig.blockHeight * 30,
+                //   // width: SizeConfig.blockWidth * 100,
+                //   // margin: EdgeInsets.only(top: SizeConfig.blockHeight * 90),
+                //   decoration: BoxDecoration(
+                //     image: DecorationImage(
+                //       scale: 0.6,
+                //       image: AssetImage('assets/images/bgbuilding.png'),
+                //     ),
+                //   ),
+                // ),
+                Container(
+                  height: SizeConfig.blockHeight * 35,
+                  margin: EdgeInsets.only(top: SizeConfig.blockHeight * 6.2),
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('assets/images/building.png'),
+                    ),
+                  ),
+                ),
+                Container(
+                  height: SizeConfig.blockHeight * 24,
+                  margin: EdgeInsets.only(top: SizeConfig.blockHeight * 20),
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('assets/images/location.png'),
+                    ),
+                  ),
+                ),
+                Container(
+                  height: SizeConfig.blockHeight * 36,
+                  margin: EdgeInsets.only(top: SizeConfig.blockHeight * 38),
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('assets/images/text.png'),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
